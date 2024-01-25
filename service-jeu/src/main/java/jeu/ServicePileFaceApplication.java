a20d1af26ab21caad019b5db8f6e3c6ccbe13497
Saiah Nadir <nadirsaiah97@gmail.com>
2024-01-25 19:06:03 +0100

    Service Jeu OK - gateway et ajout consul toujours en cours

---
+++ service-jeu/src/main/java/jeu/ServicePileFaceApplication.java
@@ -0,0 +1,13 @@
+package jeu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicePileFaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServicePileFaceApplication.class, args);
+    }
+
+}
